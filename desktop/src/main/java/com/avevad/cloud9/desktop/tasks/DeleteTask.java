ccae2c155e1604c8e6b9557eed08d62fe0bf2edf
Vadim Averin <avevad0808@gmail.com>
2020-10-21 14:52:44 +0500

    [desktop] optimized deletion task

--- desktop/src/main/java/com/avevad/cloud9/desktop/tasks/DeleteTask.java
+++ desktop/src/main/java/com/avevad/cloud9/desktop/tasks/DeleteTask.java
@@ -1,10 +1,11 @@
 package com.avevad.cloud9.desktop.tasks;
 
 import com.avevad.cloud9.core.CloudClient;
-import com.avevad.cloud9.core.util.Holder;
 
 import javax.swing.*;
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
 import static com.avevad.cloud9.core.CloudCommon.*;
 import static com.avevad.cloud9.desktop.DesktopCommon.STRING_REMOVING;
@@ -12,8 +13,7 @@ import static com.avevad.cloud9.desktop.DesktopCommon.string;
 
 public class DeleteTask extends SimpleTaskBase {
     private Node[] nodes;
-    private int filesCount = 0;
-    private int filesRemoved = 0;
+    private List<Node> nodesToDelete = new LinkedList<>();
 
     public DeleteTask(CloudClient client, Node[] nodes) {
         super(client);
@@ -21,36 +21,28 @@ public class DeleteTask extends SimpleTaskBase {
     }
 
     public void run() throws IOException, CloudClient.RequestException {
-        for (Node node : nodes) filesCount += countFiles(node);
-        if (isCancelled()) return;
-        for (Node node : nodes) deleteFiles(node);
-    }
-
-    private int countFiles(Node node) throws IOException, CloudClient.RequestException {
-        waitResume();
-        if (isCancelled()) return 0;
-        byte type = client.getNodeInfo(node).type;
-        if (type == NODE_TYPE_DIRECTORY) {
-            Holder<Integer> filesCount = new Holder<>(1);
-            client.listDirectory(node, (child, name) -> filesCount.value += countFiles(child));
-            return filesCount.value;
-        } else if (type == NODE_TYPE_FILE) return 1;
-        else return 0;
-    }
-
-    private void deleteFiles(Node node) throws IOException, CloudClient.RequestException {
-        waitResume();
-        if (isCancelled()) return;
-        byte type = client.getNodeInfo(node).type;
-        if (type == NODE_TYPE_DIRECTORY) {
-            client.listDirectory(node, (child, name) -> deleteFiles(child));
+        for (Node node : nodes) countFiles(node);
+        int removed = 0;
+        for (Node node : nodesToDelete) {
+            waitResume();
+            if (isCancelled()) return;
             client.removeNode(node);
-        } else if (type == NODE_TYPE_FILE) client.removeNode(node);
-        else return;
-        SwingUtilities.invokeLater(() -> {
-            filesRemoved++;
-            callback.setProgress((double) filesRemoved / filesCount);
-            callback.setStatus(string(STRING_REMOVING, filesRemoved, filesCount));
-        });
+            removed++;
+            int finalRemoved = removed;
+            SwingUtilities.invokeLater(() -> {
+                callback.setProgress((double) finalRemoved / nodesToDelete.size());
+                callback.setStatus(string(STRING_REMOVING, finalRemoved, nodesToDelete.size()));
+            });
+        }
+    }
+
+    private void countFiles(Node node) throws IOException, CloudClient.RequestException {
+        waitResume();
+        if (isCancelled()) return;
+        byte type = client.getNodeInfo(node).type;
+        if (type == NODE_TYPE_DIRECTORY) {
+            client.listDirectory(node, (child, name) -> countFiles(child));
+            nodesToDelete.add(node);
+        } else if (type == NODE_TYPE_FILE) nodesToDelete.add(node);
     }
 }
